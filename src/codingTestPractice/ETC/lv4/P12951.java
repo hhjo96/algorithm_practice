b26cccfb6ce26a2d4666fc0fa8d889b0b98be32c
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-03-19 09:52:21 +0900

    260319

--- src/codingTestPractice/ETC/lv4/P12951.java
+++ src/codingTestPractice/ETC/lv4/P12951.java
@@ -1,5 +1,10 @@
 package codingTestPractice.ETC.lv4;
 
+// https://school.programmers.co.kr/learn/courses/30/lessons/12951
+// JadenCase란 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은 소문자인 문자열입니다.
+// 단, 첫 문자가 알파벳이 아닐 때에는 이어지는 알파벳은 소문자로 쓰면 됩니다. (첫 번째 입출력 예 참고)
+//문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수, solution을 완성해주세요.
+
 public class P12951 {
     public static void main(String[] args) {
         String s = "3people unFollowed me";
