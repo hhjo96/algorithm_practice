909a7bb17266882c8a61abcbde2c17164a65872c
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-06-25 22:09:57 +0900

    260625

---
+++ src/codingTestPractice/ETC/lv5/P81302.java
@@ -0,0 +1,115 @@
+package codingTestPractice.ETC.lv5;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/81302
+// 로나 바이러스 감염 예방을 위해 응시자들은 거리를 둬서 대기를 해야하는데 개발 직군 면접인 만큼
+//아래와 같은 규칙으로 대기실에 거리를 두고 앉도록 안내하고 있습니다.
+//
+//대기실은 5개이며, 각 대기실은 5x5 크기입니다.
+//거리두기를 위하여 응시자들 끼리는 맨해튼 거리1가 2 이하로 앉지 말아 주세요.
+//단 응시자가 앉아있는 자리 사이가 파티션으로 막혀 있을 경우에는 허용합니다.
+// 5개의 대기실을 본 죠르디는 각 대기실에서 응시자들이 거리두기를 잘 기키고 있는지 알고 싶어졌습니다.
+// 자리에 앉아있는 응시자들의 정보와 대기실 구조를 대기실별로 담은 2차원 문자열 배열
+// places가 매개변수로 주어집니다. 각 대기실별로 거리두기를 지키고 있으면 1을,
+// 한 명이라도 지키지 않고 있으면 0을 배열에 담아 return 하도록 solution 함수를 완성해 주세요.
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class P81302 {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) {
+
+        String[][] places = {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
+                {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"}, {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"}, {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
+
+        System.out.println(Arrays.toString(solution(places)));
+    }
+
+    public static int[] solution(String[][] places) {
+
+        int[] answer = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+
+            char[][] map = new char[5][5];
+
+            for (int j = 0; j < 5; j++) {
+                map[j] = places[i][j].toCharArray();
+            }
+
+            answer[i] = check(map);
+        }
+
+        return answer;
+    }
+
+    public static int check(char[][] map) {
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+
+                if (map[i][j] == 'P') {
+
+                    if (!bfs(i, j, map)) {
+                        return 0;
+                    }
+
+                }
+            }
+        }
+
+        return 1;
+    }
+
+    public static boolean bfs(int x, int y, char[][] map) {
+
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[5][5];
+
+        queue.offer(new int[]{x, y, 0});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+
+            int[] cur = queue.poll();
+
+            int cx = cur[0];
+            int cy = cur[1];
+            int dist = cur[2];
+
+            if (dist >= 2) {
+                continue;
+            }
+
+            for (int i = 0; i < 4; i++) {
+
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) {
+                    continue;
+                }
+
+                if (visited[nx][ny]) {
+                    continue;
+                }
+
+                if (map[nx][ny] == 'X') {
+                    continue;
+                }
+
+                if (map[nx][ny] == 'P') {
+                    return false;
+                }
+
+                visited[nx][ny] = true;
+                queue.offer(new int[]{nx, ny, dist + 1});
+            }
+        }
+
+        return true;
+    }
+}
