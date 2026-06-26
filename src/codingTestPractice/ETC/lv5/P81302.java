c24cdb08bb322489df67cb8b47700707ec945c7e
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-06-26 18:53:02 +0900

    260626

--- src/codingTestPractice/ETC/lv5/P81302.java
+++ src/codingTestPractice/ETC/lv5/P81302.java
@@ -17,6 +17,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class P81302 {
+    // 세로로 짝
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
@@ -70,14 +71,18 @@ public class P81302 {
 
     public static boolean bfs(int x, int y, char[][] map) {
 
+        // x좌표, y좌표, 현재 거리
         Queue<int[]> queue = new LinkedList<>();
+        // 방문 여부
         boolean[][] visited = new boolean[5][5];
 
+        // 현재 위치를 큐에 넣기
         queue.offer(new int[]{x, y, 0});
         visited[x][y] = true;
 
         while (!queue.isEmpty()) {
 
+            // 큐에서 하나 꺼내기
             int[] cur = queue.poll();
 
             int cx = cur[0];
@@ -88,11 +93,13 @@ public class P81302 {
                 continue;
             }
 
+            // 네방향 탐색(위아래 왼오)
             for (int i = 0; i < 4; i++) {
 
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
 
+                // 유효한 범위인지 확인(배열 범위, 방문여부, 칸막이, 사람이 있는지)
                 if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) {
                     continue;
                 }
@@ -109,16 +116,19 @@ public class P81302 {
                     return false;
                 }
 
+                // 다음 자리 탐색
                 visited[nx][ny] = true;
                 queue.offer(new int[]{nx, ny, dist + 1});
             }
         }
 
+        // (x, y)에 있는 사람이 거리두기를 잘 지키고 있다면 true
         return true;
     }
 
 
     // 2번 방법
+    // 모든 p끼리 거리 계산해서 2 이하인 경우 규칙에 맞는지 확인
     public static int[] solution2(String[][] places) {
 
         int[] answer = new int[5];
@@ -139,14 +149,17 @@ public class P81302 {
 
     public static int check2(char[][] map) {
 
+        // 첫번째 p 선택
         for (int x1 = 0; x1 < 5; x1++) {
             for (int y1 = 0; y1 < 5; y1++) {
 
                 if (map[x1][y1] != 'P') continue;
 
+                // 두번째 p 선택
                 for (int x2 = 0; x2 < 5; x2++) {
                     for (int y2 = 0; y2 < 5; y2++) {
 
+                        // 자기자신 제외, p가 아니면 제외
                         if (x1 == x2 && y1 == y2) continue;
                         if (map[x2][y2] != 'P') continue;
 
@@ -154,12 +167,12 @@ public class P81302 {
 
                         if (dist > 2) continue;
 
-                        // 거리 1이면 무조건 실패
+                        // 거리 1이면 무조건 실패 PP 가로로 연속되거나 세로로 연속되는 경우
                         if (dist == 1) {
                             return 0;
                         }
 
-                        // 같은 행
+                        // 같은 행이면 P O P인지 확인
                         if (x1 == x2) {
 
                             int mid = (y1 + y2) / 2;
@@ -169,7 +182,7 @@ public class P81302 {
                             }
                         }
 
-                        // 같은 열
+                        // 같은 열이면 세로로 P O P인지 확인
                         else if (y1 == y2) {
 
                             int mid = (x1 + x2) / 2;
@@ -180,6 +193,8 @@ public class P81302 {
                         }
 
                         // 대각선
+                        // P O
+                        // O P 형태여야 함
                         else {
 
                             if (map[x1][y2] != 'X' || map[x2][y1] != 'X') {
